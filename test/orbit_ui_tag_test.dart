34d2d399a924aea40ca0cd9f723cb1275b93e8cd
Igor Gonçalves <igor_goncalves@live.com>
2022-04-29 14:19:18 -0300

    test: render box

--- test/orbit_ui_tag_test.dart
+++ test/orbit_ui_tag_test.dart
@@ -1,1 +1,19 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tag_ui/tag_ui.dart';
+
+void main() {
+  testWidgets("Render Box test", (WidgetTester tester) async {
+    const widget = TagBox(
+      minHeight: 50,
+      minWidth: 50,
+      child: Text("Render", textDirection: TextDirection.ltr),
+    );
+
+    await tester.pumpWidget(widget);
+
+    final titleFinder = find.text('Render');
+
+    expect(titleFinder, findsOneWidget);
+  });
+}
