d3d491ddb4f8a48e11dee9352e385ccaa2ea92c0
Tori-IPTI <tori.ti@ipti.org.br>
2022-07-28 12:00:47 -0300

    feat: changing checkbox event to add label

--- lib/src/components/checkbox/tag_checkbox.dart
+++ lib/src/components/checkbox/tag_checkbox.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:tag_ui/tag_ui.dart';
 
-class TagCheckbox extends StatelessWidget {
+class TagCheckbox extends StatefulWidget {
+  /// Checkbox Widget
+  ///
+  /// is requeired use [label] and the event of change [onChanged]
+
   const TagCheckbox({
     Key? key,
     required this.onChanged,
@@ -13,20 +17,47 @@ class TagCheckbox extends StatelessWidget {
   final void Function(bool? isChecked) onChanged;
   final bool? value;
   final bool disabled;
+
+  /// Texto ao lado do checkbox
   final String label;
 
   @override
+  State<TagCheckbox> createState() => _TagCheckboxState();
+}
+
+class _TagCheckboxState extends State<TagCheckbox> {
+  bool _value = false;
+
+  @override
+  void initState() {
+    _value = widget.value ?? false;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Checkbox(
-          value: value ?? false,
-          onChanged: disabled ? null : onChanged,
-        ),
-        Expanded(
-          child: TagLabel(label),
-        ),
-      ],
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _value = !_value;
+        });
+        if (!widget.disabled) widget.onChanged(_value);
+      },
+      child: Row(
+        children: [
+          Checkbox(
+            value: _value,
+            onChanged: (value) {
+              setState(() {
+                _value = value!;
+              });
+            },
+          ),
+          Expanded(
+            child: TagLabel(widget.label),
+          ),
+        ],
+      ),
     );
   }
 }
